91458c7d1bc002ed4ede928691872427bce0494c
fuadhossainnishad <fuadhossainbk01@gmail.com>
2024-12-14 07:05:20 +0600

    add root

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['localhost',process.env.NEXT_PUBLIC_API_BASE_URL_DEPLOY!]
+    domains: ['render.com',process.env.NEXT_PUBLIC_API_BASE_URL_DEPLOY!]
   },
 };
 
