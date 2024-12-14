374a1adac35f8812d6604d71e6961bc4da67c5d1
fuadhossainnishad <fuadhossainbk01@gmail.com>
2024-12-14 21:26:36 +0600

    resolve root

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['render.com',process.env.NEXT_PUBLIC_API_BASE_URL_DEPLOY!]
+    domains: ['vercel.app',process.env.NEXT_PUBLIC_API_BASE_URL_DEPLOY!]
   },
 };
 
